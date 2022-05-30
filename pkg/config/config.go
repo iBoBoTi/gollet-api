a5169fefa0aeb574d69327174ab09c5e21911c65
iBoBoTi <iroegbucalistus@gmail.com>
2022-05-30 15:20:10 +0100

    feat: add the database config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"os"
+	"time"
+)
+
+type Config struct {
+	DBHost     string
+	DBName     string
+	DBPort     string
+	DBDriver   string
+	DBUser     string
+	DBPassword string
+
+	CtxTimeout time.Duration
+}
+
+func NewConfig() *Config {
+	return &Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBName:     os.Getenv("DB_DATABASE"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBDriver:   os.Getenv("DB_DRIVER"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+	}
+}
