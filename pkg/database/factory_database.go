87407427a55d1e117e703fe2d7f83013ebf04714
iBoBoTi <iroegbucalistus@gmail.com>
2022-05-30 15:19:31 +0100

    feat: set up the database connection

---
+++ pkg/database/factory_database.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"errors"
+	"github.com/iBoBoTi/gollet-api/pkg/config"
+	"github.com/jackc/pgx/pgxpool"
+)
+
+type DB struct {
+	Postgres *pgxpool.Pool
+}
+
+var (
+	errInvalidDatabaseInstance = errors.New("invalid db instance")
+)
+
+const (
+	InstancePostgres int = iota
+)
+
+// NewDatabaseFactory returns Db type based of the db instance provided
+func NewDatabaseFactory(instance int) (*DB, error) {
+	switch instance {
+	case InstancePostgres:
+		return ConnectPostgres(config.NewConfig())
+	default:
+		return nil, errInvalidDatabaseInstance
+	}
+}
---
+++ pkg/database/postgres.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"github.com/iBoBoTi/gollet-api/pkg/config"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/sirupsen/logrus"
+)
+
+// ConnectPostgres connects to the postgres database pool and assigns it the Db struct pool field returning Db
+func ConnectPostgres(c *config.Config) (*DB, error) {
+	logrus.Info("Connecting to PostgreSQL DB pool")
+	dns := fmt.Sprintf(
+		"host=%s port=%s user=%s dbname=%s sslmode=disable password=%s",
+		c.DBHost,
+		c.DBPort,
+		c.DBUser,
+		c.DBName,
+		c.DBPassword,
+	)
+	conf, err := pgxpool.ParseConfig(dns)
+	if err != nil {
+		return nil, err
+	}
+
+	pool, err := pgxpool.ConnectConfig(context.Background(), conf)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DB{Postgres: pool}, nil
+}
