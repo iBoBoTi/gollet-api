0e8f1790dd7a79d1c1ecb38e5344a64b6de1f2c9
iBoBoTi <iroegbucalistus@gmail.com>
2022-05-31 21:01:33 +0100

    feat: implement the user repository interface

--- internal/adapters/repository/psql/user.go
+++ internal/adapters/repository/psql/user.go
@@ -1,1 +1,19 @@
 package psql
+
+import (
+	"github.com/iBoBoTi/gollet-api/internal/core/ports"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type userRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewUserRepository(db *pgxpool.Pool) ports.UserRepository {
+	return &userRepository{
+		db: db,
+	}
+}
+
+func (u *userRepository) CreateUser() {}
+func (u *userRepository) LoginUser()  {}
