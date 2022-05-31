d30b735be8fe93ee2ea27da631d269eca4e4d4af
iBoBoTi <iroegbucalistus@gmail.com>
2022-05-31 20:59:03 +0100

    feat: add the user interfaces for the user

--- internal/core/ports/user.go
+++ internal/core/ports/user.go
@@ -1,1 +1,18 @@
 package ports
+
+import "github.com/gin-gonic/gin"
+
+type UserHandler interface {
+	CreateUser(c *gin.Context)
+	LoginUser(c *gin.Context)
+}
+
+type UserService interface {
+	CreateUser()
+	LoginUser()
+}
+
+type UserRepository interface {
+	CreateUser()
+	LoginUser()
+}
