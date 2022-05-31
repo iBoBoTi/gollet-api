3f757ffcc888369759d1eded1790b1431d9b72ed
iBoBoTi <iroegbucalistus@gmail.com>
2022-05-31 21:02:27 +0100

    feat: implement the user handler interface

--- internal/adapters/api/handler/user.go
+++ internal/adapters/api/handler/user.go
@@ -1,1 +1,20 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/iBoBoTi/gollet-api/internal/core/ports"
+)
+
+type userHandler struct {
+	userService ports.UserService
+}
+
+func NewUserHandler(userService ports.UserService) ports.UserHandler {
+	return &userHandler{
+		userService: userService,
+	}
+}
+
+func (u *userHandler) CreateUser(c *gin.Context) {}
+
+func (u *userHandler) LoginUser(c *gin.Context) {}
