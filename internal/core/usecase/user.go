3ff27e02d80638653747fa29713bd4bf6d75bfb0
iBoBoTi <iroegbucalistus@gmail.com>
2022-05-31 21:00:00 +0100

    feat: implement the user service interface

--- internal/core/usecase/user.go
+++ internal/core/usecase/user.go
@@ -1,1 +1,17 @@
 package usecase
+
+import "github.com/iBoBoTi/gollet-api/internal/core/ports"
+
+type userService struct {
+	userRepository ports.UserRepository
+}
+
+func NewUserService(userRepo ports.UserRepository) ports.UserService {
+	return &userService{
+		userRepository: userRepo,
+	}
+}
+
+func (u *userService) CreateUser() {}
+
+func (u *userService) LoginUser() {}
