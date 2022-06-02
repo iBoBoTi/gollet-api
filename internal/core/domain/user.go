27edd6c4248522fb29802e00cbcd3b3f7d79dd69
iBoBoTi <iroegbucalistus@gmail.com>
2022-06-02 13:43:59 +0100

    feat: add the user password hash logic

--- internal/core/domain/user.go
+++ internal/core/domain/user.go
@@ -1,9 +1,36 @@
 package domain
 
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	BaseModel
 	Name           string `json:"name" binding:"required"`
 	Email          string `json:"email" binding:"required, email"`
 	Password       string `json:"password" binding:"required"`
-	HashedPassword string `json:"-,omitempty"`
+	HashedPassword []byte `json:"-,omitempty"`
+}
+
+func (u *User) SetPassword(password string) error {
+	hash, err := HashPassword(password)
+	if err != nil {
+		return err
+	}
+	u.HashedPassword = hash
+	return nil
+}
+
+func HashPassword(password string) ([]byte, error) {
+	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func (u *User) VerifyPassword(password string) error {
+	if u.HashedPassword != nil && len(u.HashedPassword) == 0 {
+		// Internal Server
+		return errors.New("password is not set")
+	}
+	// Wrong Password
+	return bcrypt.CompareHashAndPassword(u.HashedPassword, []byte(password))
 }
