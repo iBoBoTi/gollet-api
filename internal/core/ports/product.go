d23442bd14b76043c7056a2a29cb127ba8cc7dbc
iBoBoTi <iroegbucalistus@gmail.com>
2022-06-05 12:47:44 +0100

    feat: add the product handler, product repository, product service interfaces

--- internal/core/ports/product.go
+++ internal/core/ports/product.go
@@ -1,1 +1,24 @@
 package ports
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/iBoBoTi/gollet-api/internal/core/domain"
+)
+
+type ProductHandler interface {
+	GetProductByID(c *gin.Context)
+	GetProductsList(c *gin.Context)
+	CreateProduct(c *gin.Context)
+}
+
+type ProductService interface {
+	GetProductByID(id int64) (*domain.Product, error)
+	GetProductsList(page int) (*domain.Paginate, error)
+	CreateProduct(product *domain.Product) (*domain.Product, error)
+}
+
+type ProductRepository interface {
+	GetProductByID(id int64) (*domain.Product, error)
+	GetProductsList(paginator *domain.Paginate) (*domain.Paginate, error)
+	CreateProduct(product *domain.Product) (*domain.Product, error)
+}
