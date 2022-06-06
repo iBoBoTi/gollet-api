66f3ef2f1e0fe941ca2c20479b3c42e7249aca93
iBoBoTi <iroegbucalistus@gmail.com>
2022-06-06 23:45:27 +0100

    feat: add the credit user wallet and debit user wallet to the user interfaces

--- internal/core/ports/user.go
+++ internal/core/ports/user.go
@@ -8,15 +8,22 @@ import (
 type UserHandler interface {
 	SignUpUser(c *gin.Context)
 	LoginUser(c *gin.Context)
+	CreditUserWallet(c *gin.Context)
+	DebitUserWallet(c *gin.Context)
 }
 
 type UserService interface {
 	SignUpUser(user *domain.User) (*domain.User, error)
 	LoginUser(user *domain.LoginUserRequest) (*domain.LoginUserResponse, error)
+	CreditUserWallet(userID, amount int64) (*domain.Wallet, error)
+	DebitUserWallet(userID, amount int64) (*domain.Wallet, error)
 }
 
 type UserRepository interface {
 	SignUpUser(user *domain.User) (*domain.User, error)
 	GetUserByEmail(email string) (*domain.User, error)
 	CreateUserWallet(wallet *domain.Wallet) (*domain.Wallet, error)
+	GetUserWallet(userID int64) (*domain.Wallet, error)
+	CreditUserWallet(userID, amount int64) (*domain.Wallet, error)
+	DebitUserWallet(userID, amount int64) (*domain.Wallet, error)
 }
