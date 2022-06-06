d218d52bb8f9e5e2af725a277ac773d3f3b46566
iBoBoTi <iroegbucalistus@gmail.com>
2022-06-06 23:46:59 +0100

    feat: implement the credit user wallet and debit wallet methods for the user repository interface

--- internal/adapters/repository/psql/user.go
+++ internal/adapters/repository/psql/user.go
@@ -48,3 +48,35 @@ func (u *userRepository) GetUserByEmail(email string) (*domain.User, error) {
 	}
 	return &user, nil
 }
+
+func (u *userRepository) GetUserWallet(userID int64) (*domain.Wallet, error) {
+	wallet := domain.Wallet{}
+	queryString := `SELECT * FROM wallets WHERE user_id = $1`
+	err := u.db.QueryRow(context.Background(), queryString, userID).Scan(&wallet.ID, &wallet.UserID, &wallet.Balance, &wallet.Currency, &wallet.CreatedAt, &wallet.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return &wallet, nil
+}
+
+func (u *userRepository) CreditUserWallet(userID, amount int64) (*domain.Wallet, error) {
+	queryString := `UPDATE wallets SET balance = $1  WHERE user_id = $2 RETURNING *`
+	result := &domain.Wallet{}
+	row := u.db.QueryRow(context.Background(), queryString, amount, userID)
+	err := row.Scan(&result.ID, &result.UserID, &result.Balance, &result.Currency, &result.CreatedAt, &result.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
+
+func (u *userRepository) DebitUserWallet(userID, amount int64) (*domain.Wallet, error) {
+	queryString := `UPDATE wallets SET balance = $1  WHERE user_id = $2 RETURNING *`
+	result := &domain.Wallet{}
+	row := u.db.QueryRow(context.Background(), queryString, amount, userID)
+	err := row.Scan(&result.ID, &result.UserID, &result.Balance, &result.Currency, &result.CreatedAt, &result.UpdatedAt)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
